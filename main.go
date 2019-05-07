6d92ca681bd4588d024c2652de59888063879f5d
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-05-06 19:14:20 -0700

    connect search to classify oclc, it's slow to load

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"html/template"
 	"net/http"
+	"html/template"
+
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+
 	"encoding/json"
+	"net/url"
+	"io/ioutil"
+	"encoding/xml"
 )
 
 type Page struct {
-	Name     string
+	Name string
 	DBStatus bool
 }
 
 type SearchResult struct {
-	Title string
-	Author string
-	Year string
-	ID string
+	Title string `xml:"title,attr"`
+	Author string `xml:"author,attr"`
+	Year string `xml:"hyr,attr"`
+	ID string `xml:"owi,attr"`
 }
 
 func main() {
@@ -31,27 +36,49 @@ func main() {
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
-
 		p.DBStatus = db.Ping() == nil
 
 		if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		//db.Close()
 	})
 
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		results := []SearchResult {
-			SearchResult{ "Moby Dick", "Herman Melville", "1851", "222222"},
-			SearchResult{ "Adventure of Huck Fin", "Mark Twain", "1884", "444444"},
-			SearchResult{ "The Catcher in the Rye", "JD Salinger", "1951", "333333"},
+		var results []SearchResult
+		var err error
+
+		if results, err = search(r.FormValue("search")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 
 		encoder := json.NewEncoder(w)
-		if err := encoder.Encode(results); err !=nil {
+		if err := encoder.Encode(results); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
-	fmt.Println(http.ListenAndServe(":8080", nil))
 
+	fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+type ClassifySearchResponse struct {
+	Results []SearchResult `xml:"works>work"`
+}
+
+func search(query string) ([]SearchResult, error) {
+	var resp *http.Response
+	var err error
+
+	if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?&summary=true&title=" + url.QueryEscape(query)); err != nil {
+		return []SearchResult{}, err
+	}
+
+	defer resp.Body.Close()
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		return []SearchResult{}, err
+	}
+
+	var c ClassifySearchResponse
+	err = xml.Unmarshal(body, &c)
+	return c.Results, err
 }
